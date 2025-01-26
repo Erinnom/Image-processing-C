77c62b4c1861d4920ace4b191d0cdce75d7d10fb
Erinnom <4uei565o@duck.com>
2025-01-26 16:39:35 +0100

    grayscale implement

--- main.c
+++ main.c
@@ -2,36 +2,27 @@
 Objective : load image, edit image and save edited image.
 */
 
-
 #include <stdio.h>
 #include "ti.h"
-int main(int argc, char **argv){
-    // Tests
-    char * file_name = "luffy2.png";
-    union image image1;
-    load_bmp_image(file_name,&image1);
 
-    printf("Height %d\n",image1.png.header.height);
-    printf("Width %d\n",image1.png.header.width);
-
-    for (int i = 0;i < 200;i++){
-        if (i % 20 == 19){
-            printf(" %3d\n",image1.raw[i]);
-        }else {
-            printf(" %3d",image1.raw[i]);
+void grayscale(union image *image) {
+    for (int i = 0; i < image->bmp.width; i++){
+        for(int j = 0; j < image->bmp.height; j++){
+            struct pixel pixel;
+            get_pixel(image,i,j,&pixel);
+            pixel.blue = (pixel.blue + pixel.green + pixel.red)/3;
+            pixel.green = pixel.blue;
+            pixel.red = pixel.blue;
+            set_pixel(image,i,j,&pixel);
         }
     }
-    // int k = 0;
-    // printf("k %2x\n",&k);
-    printf("data %2x <- %2x\n",&image1.raw,image1.raw[0]);
-    printf("Signature %2x <- %8s\n",&image1.png.header.signature,image1.png.header.signature);
-    printf("File size %2x <- %2x\n",&image1.png.header.length,image1.png.header.length);
-    printf("Type %2x <- %2s\n",&image1.png.header.type,image1.png.header.type);
-    printf("Width %2x <- %2d\n",&image1.png.header.width,image1.png.header.width>>24);
-    printf("Height %2x <- %2d\n",&image1.png.header.height,image1.png.header.height>>24);
+}
 
-
-
-
-
+int main(int argc, char **argv){
+    // Tests
+    char * file_name = "luffy2.bmp";
+    union image image1;
+    load_bmp_image(file_name,&image1);
+    grayscale(&image1);
+    save_bmp_image("luffy2-gray.bmp",&image1);
 }
--- ti.h
+++ ti.h
@@ -3,63 +3,49 @@
 
 #include <stdio.h>
 
-union image{
-    struct bmp {
-        struct bmp_header {
-            unsigned short draft; // add an offset of 2 bytes to avoid byte de-alignment
-            unsigned char signature[2];
-            unsigned int file_size;
-            unsigned short reserved1;
-            unsigned short reserved2;
-            unsigned int offset;
-            unsigned int header_size;
-            unsigned int width;
-            unsigned int height;
-            unsigned short planes;
-            unsigned short bits_per_pixel;
-            unsigned int compression;
-            unsigned int image_size;
-            unsigned int x_pixels_per_meter;
-            unsigned int y_pixels_per_meter;
-            unsigned int colors_used;
-            unsigned int important_colors;
-            unsigned int red_mask;
-            unsigned int green_mask;
-            unsigned int blue_mask;
-            unsigned int alpha_mask;
-            unsigned int color_space_type;
-            unsigned int space_endpoints;
-            unsigned int gamma_red;
-            unsigned int gamma_green;
-            unsigned int gamma_blue;
-            unsigned int intent;
-            unsigned int profile_data;
-            unsigned int profile_size;
-            unsigned int reserved;
-        } header;
-        struct data{
-            unsigned char data;
-        } data;
-    } bmp;
+union image {
+  struct bmp {
+    unsigned short draft; // add an offset of 2 bytes to avoid byte de-alignment
+    unsigned char signature[2];
+    unsigned int file_size;
+    unsigned short reserved1;
+    unsigned short reserved2;
+    unsigned int offset;
+    unsigned int header_size;
+    unsigned int width;
+    unsigned int height;
+    unsigned short planes;
+    unsigned short bits_per_pixel;
+    unsigned int compression;
+    unsigned int image_size;
+    unsigned int x_pixels_per_meter;
+    unsigned int y_pixels_per_meter;
+    unsigned int colors_used;
+    unsigned int important_colors;
+    unsigned int red_mask;
+    unsigned int green_mask;
+    unsigned int blue_mask;
+    unsigned int alpha_mask;
+    unsigned int color_space_type;
+    unsigned int space_endpoints;
+    unsigned int gamma_red;
+    unsigned int gamma_green;
+    unsigned int gamma_blue;
+    unsigned int intent;
+    unsigned int profile_data;
+    unsigned int profile_size;
+    unsigned int reserved;
+  } bmp;
 
-    struct png {
-        struct png_header {
-            unsigned char signature[8];
-            unsigned int length;
-            unsigned char type[4];
-            unsigned int width;
-            unsigned int height;
-            unsigned char bit_depth;
-            unsigned char color_type;
-            unsigned char compression;
-            unsigned char filter;
-            unsigned char interlace;
-        }header;
-    } png;
-
-    unsigned char raw[1024*1024]; // 1Mo
+  unsigned char raw[2 * 1024 * 1024]; // 1Mo
 };
 
+struct pixel {
+    unsigned char red;
+    unsigned char green;
+    unsigned char blue;
+    unsigned char alpha;
+};
 
 /*
 Objective : load bmp image
@@ -67,14 +53,63 @@ Input : file_name : char * : file name
         image : union bmp_image * : image
 Output : void
 */
-void load_bmp_image(char *file_name, union image *image){
-    FILE *file = fopen(file_name,"rb");
-    if(file == NULL){
-        printf("Error : file not found");
-    } else {
-        fread((image->raw),1,sizeof(*image),file);
+void load_bmp_image(char *file_name, union image *image) {
+  FILE *file = fopen(file_name, "rb");
+  if (file == NULL) {
+    printf("Error : file not found");
+  } else {
+    fread((image->raw + 2), 1, sizeof(*image), file);
+  }
+  fclose(file);
+};
+
+/*
+Objective : get pixel value from image
+Input : image : union bmp_image * : image
+        x : int : x coordinate
+        y : int : y coordinate
+
+*/
+void get_pixel(union image *image, int x, int y,struct pixel *pixel) {
+    unsigned int offset = image->bmp.offset+2 + ((image->bmp.height - y - 1)*(image->bmp.width +1)+ x)*image->bmp.bits_per_pixel/8;
+    pixel->blue = image->raw[offset];
+    pixel->green = image->raw[offset + 1];
+    pixel->red = image->raw[offset + 2];
+    if (image->bmp.bits_per_pixel == 32) {
+        pixel->alpha = image->raw[offset + 3];
     }
-    fclose(file);
+}
+
+/*
+Objective : set pixel value from image
+Input : image : union bmp_image * : image
+        x : int : x coordinate
+        y : int : y coordinate
+        pixel : struct pixel * : pixel
+*/
+void set_pixel(union image *image, int x, int y,struct pixel *pixel) {
+    unsigned int offset = image->bmp.offset+2 + ((image->bmp.height - y - 1)*(image->bmp.width +1)+ x)*image->bmp.bits_per_pixel/8;
+    image->raw[offset] = pixel->blue;
+    image->raw[offset + 1] = pixel->green;
+    image->raw[offset + 2] = pixel->red;
+    if (image->bmp.bits_per_pixel == 32) {
+        image->raw[offset + 3] = pixel->alpha;
+    }
+}
+
+/*
+Objective : save bmp image
+Input : file_name : char * : file name
+        image : union bmp_image * : image
+*/
+void save_bmp_image(char *file_name, union image *image) {
+  FILE *file = fopen(file_name, "wb");
+  if (file == NULL) {
+    printf("Error : file not found");
+  } else {
+    fwrite((image->raw + 2), 1, sizeof(*image), file);
+  }
+  fclose(file);
 };
 
 #endif
